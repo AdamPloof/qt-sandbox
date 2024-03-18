0e3500a49d689bc2b5f88184b11658212e77d473
Adam Ploof <adamploof@hotmail.com>
2024-03-18 00:16:11 -0400

    set up gtest to test application components

--- src/main.cpp
+++ src/main.cpp
@@ -11,27 +11,10 @@
 #include "service/dao.h"
 #include "entity/expense.h"
 
-void testExpense() {
-    Expense expense;
-    expense.setData("id", 42);
-    expense.setData("description", "Forty two");
-    expense.setData("amount", 4.2);
-
-    QHash<QString, QVariant> data = expense.getData();
-    QHashIterator<QString, QVariant> i(data);
-
-    while (i.hasNext()) {
-        i.next();
-
-        qDebug() << i.key() << ": " << i.value();
-    }
-}
-
 int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
     std::shared_ptr<DAO> dao = std::make_shared<DAO>();
     App app = App(dao);
-    testExpense();
     app.run();
     app.show();
 
